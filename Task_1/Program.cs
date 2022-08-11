a2c0c6f77a7cd82ca8a4c4dac75fdffd09cbbe49
Sergey <hojalata@mail.ru>
2022-08-11 23:21:42 +0300

    Доделал задачу №19

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,5 +1,6 @@
-﻿Console.Write("Введите пятизначное число: ");
-string number = Console.ReadLine();
+﻿using System;
+Console.Write("Введите пятизначное число: ");
+string? number = Console.ReadLine();
 int number1 = number[0];
 int number2 = number[1];
 int number3 = number[2];
@@ -10,7 +11,12 @@ if (number1 == number5 && number2 == number4)
 {
     Console.WriteLine("Данное число палиндром!");
 }
+else if (number.Length != 5)
+{
+    Console.WriteLine("Даное число не пятизначное");
+}
 else
 {
     Console.WriteLine("Даное число не является палиндромом");
 }
+
